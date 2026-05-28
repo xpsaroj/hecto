0564296380793a2530afff92f03f41446c2091c7
Saroj <saroj.p.2728@gmail.com>
2026-05-28 13:25:26 +0545

    refactor: split editor logic into separate module and add refine the input loop

---
+++ src/editor.rs
@@ -0,0 +1,36 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor {}
+    }
+
+    pub fn run(&self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+
+    fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+
+        loop {
+            if let Key(event) = read()? {
+                println!("{event:?} \r");
+
+                if let Char(c) = event.code
+                    && c == 'q'
+                {
+                    break;
+                }
+            }
+        }
+
+        disable_raw_mode()?;
+        Ok(())
+    }
+}
