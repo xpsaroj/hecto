cf0153636ace83d6722034b42811105e9b1b9430
Saroj <saroj.p.2728@gmail.com>
2026-06-09 21:32:48 +0545

    feat(editor): add welcome message and improve terminal command handling

--- src/editor.rs
+++ src/editor.rs
@@ -10,6 +10,9 @@ use std::io::Error;
 
 use terminal::{Position, Size, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
 }
@@ -68,12 +71,35 @@ impl Editor {
         Terminal::execute()
     }
 
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- v{VERSION}");
+
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+
+        // Computes self - rhs, saturating at the numeric bounds instead of overflowing. so that padding doesn't wrap to usize::MAX, basically (width - len) / 2 stopping at 0
+        let padding = (width.saturating_sub(len)) / 2; // we allow this as we don't care if welcome message is put exactly in the middle, may be little bit left or right (cause int div would be an int, where the actual middle may be a fraction no.)
+        let spaces = " ".repeat(padding.saturating_sub(1));
+
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")
+    }
+
     fn draw_rows() -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
-        for i in 0..height {
+        for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if i + 1 < height {
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row + 1 < height {
                 Terminal::print("\r\n")?;
             }
         }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,8 +1,9 @@
 use crossterm::cursor::{Hide, MoveTo, Show};
-use crossterm::queue;
+use crossterm::{Command, queue};
 use crossterm::style::Print;
 use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
 
+use core::fmt::Display;
 use std::io::{Error, Write, stdout};
 
 #[derive(Clone, Copy)]
@@ -33,27 +34,27 @@ impl Terminal {
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::All))
+        Self::queue_command(Clear(ClearType::All))
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))
+        Self::queue_command(Clear(ClearType::CurrentLine))
     }
 
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        queue!(stdout(), MoveTo(position.x, position.y))
+        Self::queue_command(MoveTo(position.x, position.y))
     }
 
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(stdout(), Show)
+        Self::queue_command(Show)
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
-        queue!(stdout(), Hide)
+        Self::queue_command(Hide)
     }
 
-    pub fn print(string: &str) -> Result<(), Error> {
-        queue!(stdout(), Print(string))
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))
     }
 
     pub fn size() -> Result<Size, Error> {
@@ -63,5 +64,9 @@ impl Terminal {
 
     pub fn execute() -> Result<(), Error> {
         stdout().flush()
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)
     }
 }
