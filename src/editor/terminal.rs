d2105b8bf8c71c61845a7ae09e4960ee46433d41
Saroj <saroj.p.2728@gmail.com>
2026-06-18 11:04:11 +0545

    feat(editor): add cursor movement and keyboard navigation

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,30 +1,38 @@
 use crossterm::cursor::{Hide, MoveTo, Show};
-use crossterm::{Command, queue};
 use crossterm::style::Print;
 use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::{Command, queue};
 
 use core::fmt::Display;
 use std::io::{Error, Write, stdout};
 
 #[derive(Clone, Copy)]
 pub struct Size {
-    pub height: u16,
-    pub width: u16,
+    pub height: usize,
+    pub width: usize,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 pub struct Position {
-    pub x: u16,
-    pub y: u16,
+    pub col: usize,
+    pub row: usize,
 }
 
+/// Represents the Terminal.
+/// 
+/// Edge Case for platforms where `usize`  < `u16`:
+/// 
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller
+///
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`).
+/// And should you attempt to set the caret out of these bounds, it will also be truncated.
 pub struct Terminal {}
 
 impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()
     }
 
@@ -41,15 +49,17 @@ impl Terminal {
         Self::queue_command(Clear(ClearType::CurrentLine))
     }
 
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        Self::queue_command(MoveTo(position.x, position.y))
+    /// Moves the caret to the given Position
+    /// * `Position` - the `Position` to move the caret to. Will be truncated to `u16::MAX` if bigger
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))
     }
 
-    pub fn show_cursor() -> Result<(), Error> {
+    pub fn show_caret() -> Result<(), Error> {
         Self::queue_command(Show)
     }
 
-    pub fn hide_cursor() -> Result<(), Error> {
+    pub fn hide_caret() -> Result<(), Error> {
         Self::queue_command(Hide)
     }
 
@@ -59,6 +69,8 @@ impl Terminal {
 
     pub fn size() -> Result<Size, Error> {
         let (width, height) = size()?;
+        let height = height as usize;
+        let width = width as usize;
         Ok(Size { height, width })
     }
 
