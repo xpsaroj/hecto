b15111de4c7fc569229d3da6da9dbd5654384860
Saroj <saroj.p.2728@gmail.com>
2026-08-05 22:31:40 +0545

    refactor(editor): enhance text navigation logic and add line length retrieval

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -7,6 +7,8 @@ use crate::editor::{
     terminal::{Position, Size, Terminal},
 };
 use buffer::Buffer;
+use core::cmp::min;
+use line::Line;
 use location::Location;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
@@ -76,8 +78,8 @@ impl View {
 
     fn move_text_location(&mut self, direction: &Direction) {
         let Location { mut x, mut y } = self.location;
-        let Size { height, width } = self.size;
-        
+        let Size { height, .. } = self.size;
+
         match direction {
             Direction::Up => {
                 y = y.saturating_sub(1);
@@ -86,24 +88,43 @@ impl View {
                 y = y.saturating_add(1);
             }
             Direction::Left => {
-                x = x.saturating_sub(1);
+                if x > 0 {
+                    x -= 1; // if at start of the line, move left
+                } else if y > 0 {
+                    y -= 1; // if at the start of the line (x = 0), move one line up (the line's end)
+                    x = self.buffer.lines.get(y).map_or(0, Line::len);
+                }
             }
             Direction::Right => {
-                x = x.saturating_add(1);
+                let width = self.buffer.lines.get(y).map_or(0, Line::len);
+                if x < width {
+                    x += 1; // if within the line, go right
+                } else {
+                    y = y.saturating_add(1); // if at the end of line, go one line down to next line's start
+                    x = 0;
+                }
             }
             Direction::PageUp => {
-                y = 0;
+                y = y.saturating_sub(height).saturating_sub(1);
             }
             Direction::PageDown => {
-                y = height.saturating_sub(1);
+                y = y.saturating_add(height).saturating_sub(1);
             }
             Direction::Home => {
                 x = 0;
             }
             Direction::End => {
-                x = width.saturating_sub(1);
+                x = self.buffer.lines.get(y).map_or(0, Line::len);
             }
         }
+
+        // snap x and y to valid positions (they may go off if skipped, eg: on RIGHT when on end of a line, we aren't allowed to go down if no line exists below)
+        x = self
+            .buffer
+            .lines
+            .get(y)
+            .map_or(0, |line| min(line.len(), x));
+        y = min(y, self.buffer.lines.len());
 
         self.location = Location { x, y };
         self.scroll_location_into_view();
